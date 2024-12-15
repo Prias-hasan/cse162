a4b42d75f210dcf1ef38f315a8c6895cafa51255
Prias-hasan <moktadir.hasan.004@gmail.com>
2024-12-15 18:07:21 +0600

    Add files via upload

---
+++ CSE162/problem/7.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+
+    int sum, n;
+    float b;
+    printf("inter an integer number:");
+  
+    scanf("%d",&n);
+  printf("inter a floating-point  number:");
+  scanf("%f",&b);
+   printf("sum of and%.4f is\n",n+b);
+   return 0;
+}
